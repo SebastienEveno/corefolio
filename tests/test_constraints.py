07fe13e5c584c46bffab6b7d24006f1e4e61c8bf
Sébastien Eveno <72524233+SebastienEveno@users.noreply.github.com>
2025-03-09 20:15:28 +0100

    Refactor Constraints and Optimizer classes to use instance variable for max_assets (#15)

--- tests/test_constraints.py
+++ tests/test_constraints.py
@@ -7,6 +7,7 @@ from corefolio.constraints import Constraints
 
 def test_apply_constraints():
     x = cp.Variable(3, boolean=True)
-    constraints = Constraints.apply_constraints(
-        x, max_assets=2)
-    assert len(constraints) == 1
+    constraints = Constraints(max_assets=2)
+    applied_constraints = constraints.apply_constraints(x)
+    assert len(applied_constraints) == 1
+    assert applied_constraints[0].args[1].value == 2
